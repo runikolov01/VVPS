7a9a83b7df86e6657cafc6db8ade1fc787fc1807
Rumen Nikolov <rumenhco2001@abv.bg>
2024-04-09 08:54:06 +0300

    final refactoring

--- src/main/java/InputHelper.java
+++ src/main/java/InputHelper.java
@@ -7,17 +7,20 @@ public class InputHelper {
     public static int readValidInput(Scanner scanner, String message) {
         int input;
         System.out.print(message);
-        while (!scanner.hasNextInt() || (input = scanner.nextInt()) < 2) {
+        while (true) {
             if (!scanner.hasNextInt()) {
                 System.out.println("Invalid input. Please enter an integer number.");
-                scanner.next();
-            } else {
+                scanner.nextLine();
+            } else if ((input = scanner.nextInt()) < 2) {
                 System.out.println("Please enter an integer number 2 or bigger.");
+            } else {
+                break; // Valid input received, exit the loop
             }
             System.out.print(message);
         }
         return input;
     }
+
 
 
     public static String getInputString(Scanner scanner, String message) {
